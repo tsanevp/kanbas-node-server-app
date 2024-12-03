3406df43b367b2378c53eaca602823f2518332ea
tsanevp <petertsanev7@gmail.com>
2024-12-03 12:22:26 -0800

    added logs for debugging

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -56,7 +56,9 @@ export default function UserRoutes(app) {
     };
     const signin = async (req, res) => {
         const { username, password } = req.body;
+        console.log(username);
         const currentUser = await dao.findUserByCredentials(username, password);
+        console.log(currentUser);
         if (currentUser) {
             req.session["currentUser"] = currentUser;
             res.json(currentUser);
